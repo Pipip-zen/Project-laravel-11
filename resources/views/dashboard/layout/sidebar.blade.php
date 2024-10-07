3035672b0646a352bdcfe7db4d9a4be095ad4853
Pipip-zen <rafif.nuha@gmail.com>
2024-10-07 08:58:25 +0700

    menambah dashboard index

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -4,8 +4,15 @@
       <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
     </div>
+    
     <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
       <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link text-black d-flex align-items-center gap-2 {{ Request::is('dashboard/posts*') ? 'bg-black text-white' : '' }}" href="/dashboard/posts">
+              <i class="fa-solid fa-upload"></i>
+              Dashboard
+          </a>
+        </li>
         <li class="nav-item">
           <a class="nav-link text-black d-flex align-items-center gap-2 {{ Request::is('dashboard/posts*') ? 'bg-black text-white' : '' }}" href="/dashboard/posts">
               <i class="fa-solid fa-upload"></i>
@@ -13,6 +20,7 @@
           </a>
         </li>
       </ul>
+      
 
       @can('admin')
       <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
