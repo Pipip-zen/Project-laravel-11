0e66ac946b79eecec2e2bffb5fcb1d3fe5370175
Pipip-zen <rafif.nuha@gmail.com>
2024-10-16 14:50:54 +0700

    mengubah aksen warna biru

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,3 @@
 @props(['active' => false])
 
-<a {{ $attributes }} class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-600 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 mx-2 text-sm font-medium" aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
+<a {{ $attributes }} class="{{ $active ? 'bg-blue-700 text-white' : 'text-gray-600 hover:bg-blue-500 hover:text-white' }} rounded-md px-3 py-2 mx-2 text-sm font-medium" aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -9,17 +9,17 @@
         <div class="flex flex-col justify-between flex-1 mt-6">
             <nav>
 
-                <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700  {{ Request::is('dashboard') ? 'bg-black text-white' : '' }}" href="/dashboard">
-                  <i class="fa-solid fa-house"></i>
-
-                    <span class="mx-4 font-medium">Dashboard</span>
-                </a>
-
-                <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700  {{ Request::is('dashboard/posts*') ? 'bg-black text-white' : '' }}" href="/dashboard/posts">
-                  <i class="fa-solid fa-upload"></i>
-
-                    <span class="mx-4 font-medium">My Posts</span>
-                </a>
+              <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700  {{ Request::is('dashboard') ? 'bg-blue-500 text-white' : '' }}" href="/dashboard">
+                <i class="fa-solid fa-house"></i>
+              
+                <span class="mx-4 font-medium">Dashboard</span>
+              </a>
+              
+              <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700  {{ Request::is('dashboard/posts*') ? 'bg-blue-500 text-white' : '' }}" href="/dashboard/posts">
+                <i class="fa-solid fa-upload"></i>
+              
+                <span class="mx-4 font-medium">My Posts</span>
+              </a>
 
                 <hr class="my-6 border-gray-200 dark:border-gray-600" />
 
@@ -27,11 +27,12 @@
                 <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
                   <span>ADMINISTRATOR</span>
                 </h6>
-                <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700 {{ Request::is('dashboard/categories*') ? 'bg-black text-white' : '' }}" href="/dashboard/categories">
+                <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700 {{ Request::is('dashboard/categories*') ? 'bg-blue-500 text-white' : '' }}" href="/dashboard/categories">
                   <i class="fa-solid fa-list"></i>
-
-                    <span class="mx-4 font-medium">Post Categories</span>
+                
+                  <span class="mx-4 font-medium">Post Categories</span>
                 </a>
+                
                 @endcan
 
                 <form action="/logout" method="POST">
